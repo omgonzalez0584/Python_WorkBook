58639dc06fb51b2783d594074334e31ea1f3415c
omgonzalez0584 <om.gonzalez007@gmail.com>
2020-03-18 16:38:12 -0500

    Refactorizando el codigo

--- volumen_cilindro.py
+++ volumen_cilindro.py
@@ -1,12 +1,15 @@
 ##Exercise 17 Volumen de un cilindro
 import math
 
+def volumen_cilindro(radio,altura):
+    """Calcular el volumen de un cilindro."""
+    volumen  = (math.pi * (radio ** 2)) * altura
+
+    return round(volumen,2)  # retornando el resultado
+
 #Informacion ingresada por teclado
 radio = float(input('Introduzca el radio: '))
 altura = float(input('Introduzca la altura: '))
 
-#formula matematica
-volumen  = (math.pi * (radio ** 2)) * altura
-
 #imprimiendo informacion por teclado
-print('El volumen del cilindro: ' + str(round(volumen,2)))
+print('El volumen del cilindro: ' + str(volumen_cilindro(radio,altura)))
