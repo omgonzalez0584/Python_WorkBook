20d2b26eee37c2d00d87fc0b87edcb6e4d368f1f
omgonzalez0584 <om.gonzalez007@gmail.com>
2020-03-21 15:54:36 -0500

    Exercise 34 Terminado

--- Chapter2/even_or_odd.py
+++ Chapter2/even_or_odd.py
@@ -1,2 +1,10 @@
 #Exercise 34: Even or Odd?
 
+numero int(input('Introduzca un numero entero: '))
+
+if numero % 2 == 0:
+    print('The number ' + str(numero) + ' is even!')
+else:
+    print('The number ' + str(numero + ' is odd!'))
+
+
