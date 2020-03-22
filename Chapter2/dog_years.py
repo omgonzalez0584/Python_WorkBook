23ec24043ba850005922a8c555823718b6c4e839
omgonzalez0584 <om.gonzalez007@gmail.com>
2020-03-21 19:59:43 -0500

    Documentado el codigo

--- Chapter2/dog_years.py
+++ Chapter2/dog_years.py
@@ -1,6 +1,9 @@
 #Exercise 35:  Dog Year
+
+#ingresando datos por teclado
 human_years = int(input('Introduzca su edad: '))
 
+#Estructura if-else para realizar la conversion
 if human_years  > 2:
     human_years = human_years - 2
     dog_years = ( human_years * 4 ) + 10.5
@@ -14,4 +17,5 @@ elif human_years == 1:
     print('Su edad perruna es: ' + str(dog_years))
 
 else:
+    #Mensaje si el usuario ingresa un numero negativo o cero
     print('Error! , debe ingresar un numero mayor a cero.')
