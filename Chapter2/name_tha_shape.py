b81ba68bc1fefd7ed12620bc5bf6d3d540df84ad
omgonzalez0584 <om.gonzalez007@gmail.com>
2020-03-22 11:52:47 -0500

    Manejando Excepciones

--- Chapter2/name_tha_shape.py
+++ Chapter2/name_tha_shape.py
@@ -1,13 +1,20 @@
 #Exercise 37: Name that shape
 
+#Diccionario almacena las formas del 3 al 10
 shapes = {3:'Triangulo',4:'Cuadrado',5:'Pentagono',
             6:'Hexagono',7:'Heptagono',8:'Octagono',
             9:'Eneagono',10:'Decagono'}
 
-number_sides = int(input('Introduzca el numero de lados: '))
+#Estructura Try / Except para validar los datos ingresados
+try:
+#Ingresando datos por teclado
+    number_sides = int(input('Introduzca el numero de lados: '))
 
-if number_sides >= 3 and number_sides <= 10:
-    print('Es un ' + shapes[number_sides])
+#If / Else para validar el rango de numeros
+    if number_sides >= 3 and number_sides <= 10:
+        print('Es un ' + shapes[number_sides])
+    else:
+        print('Debe escribir un numero entre 3 y 10')
 
-else:
-    print('Debe escribir un numero entre 3 y 10')
+except ValueError:
+    print('Error, debe escribir un numero...')
