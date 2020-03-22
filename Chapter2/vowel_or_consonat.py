e36c1177aa99270f95fdcf4928923d700dedd641
omgonzalez0584 <om.gonzalez007@gmail.com>
2020-03-21 21:08:05 -0500

    Agregando mas expresiones regulares

--- Chapter2/vowel_or_consonat.py
+++ Chapter2/vowel_or_consonat.py
@@ -1,5 +1,5 @@
 #Exercise 36
-import re
+import re #Modulo de expresiones regulares
 
 #ingresando valores por teclado
 letter = input('Introduzca la letra: ')
@@ -8,10 +8,13 @@ letter = letter.lower()
 #Estructura de control que valida
 #Si letter es una vocal o consonante
 if re.match('[aeiou]' , letter):
-    print(letter + ' es una vocal')
+    print(letter.title()  + ' es una vocal')
 
 elif letter == 'y':
-    print(letter + ' es aveces una vocal o una consonante')
+    print(letter.title()  + ' es aveces una vocal o una consonante')
+
+elif re.match('[0-9]',letter) or re.match('[!~@#$%^&*()_+]',letter):
+    print('Error, debe escribir una letra')
 
 else:
-    print(letter + ' es una consonante')
+    print(letter.title() + ' es consonante')
