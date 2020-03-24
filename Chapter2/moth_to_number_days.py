45a93a8d3c6879367034448d97ee584aa38c5914
omgonzalez0584 <om.gonzalez007@gmail.com>
2020-03-24 11:34:01 -0500

    Agregando validaciones por teclado

--- Chapter2/moth_to_number_days.py
+++ Chapter2/moth_to_number_days.py
@@ -1,18 +1,23 @@
 #Exercise 38 Month Days to a Number Days
 
+#Ingresando mes
 month = input('Introduzca el mes: ')
 month = month.lower()
 
+#Estructura if/else para validar dias
 if (month == 'abril' or month == 'junio' or
     month == 'septiembre' or month == 'noviembre'):
 
-    print(month + ' tiene 30 dias')
+    print(month.title() + ' tiene 30 dias')
 
 elif (month == 'enero' or month == 'marzo'
     or month == 'mayo' or month == 'julio' or month == 'agosto'
     or month == 'octubre' or month == 'diciembre'):
 
-    print(month + ' tiene 31 dias')
+    print(month.title() + ' tiene 31 dias')
 
-else:
-    print(month + ' tiene 28 o 29 dias')
+elif month == 'febrero':
+    print(month.title() + ' tiene 28 o 29 dias')
+
+else: #Utilizado para cualquier otro dato incorrecto
+    print('Debe ingresar un mes..!')
