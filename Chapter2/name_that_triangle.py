418d1fcaec71e64a34f9a41591356d01b280d695
omgonzalez0584 <om.gonzalez007@gmail.com>
2020-03-26 12:40:25 -0500

    Aplicando Try/Except

--- Chapter2/name_that_triangle.py
+++ Chapter2/name_that_triangle.py
@@ -1,14 +1,17 @@
 #Exercise 40 Name of triangle
 
-side_a = float(input('Introduzca el lado A: '))
-side_b = float(input('Introduzca el lado B: '))
-side_c = float(input('Introduzca el lado C: '))
+try:
+    side_a = float(input('Introduzca el lado A: '))
+    side_b = float(input('Introduzca el lado B: '))
+    side_c = float(input('Introduzca el lado C: '))
 
-if side_a == side_b and side_a == side_c:
-    print('El triangulo es Equilatero.')
+    if side_a == side_b and side_a == side_c:
+        print('El triangulo es Equilatero.')
 
-elif side_a != side_b and side_a != side_c  and side_b != side_c:
-    print('El triangulo es Escaleno.')
+    elif side_a != side_b and side_a != side_c  and side_b != side_c:
+        print('El triangulo es Escaleno.')
 
-else:
-    print('El triangulo es Isosceles.')
+    else:
+        print('El triangulo es Isosceles.')
+except ValueError:
+    print('Error!, Debe ingresar valores numericos..')
