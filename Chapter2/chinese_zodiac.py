d40edbef354459035aae60391d1aae0ab2d51575
omgonzalez0584 <om.gonzalez007@gmail.com>
2020-04-02 21:41:30 -0500

    Exercise 48

---
+++ Chapter2/chinese_zodiac.py
@@ -0,0 +1,31 @@
+#Exercise 48 Chinese Zodiac
+
+
+year = int(input('Input your year birthday: '))
+
+if year % 12 == 8:
+    print('Dragon')
+elif year % 12 == 9:
+    print('Snake')
+elif year % 12 == 10:
+    print('Horse')
+elif year % 12 == 11:
+    print('Sheep')
+elif year % 12  == 0:
+    print('Monkey')
+elif year % 12 == 1:
+    print('Rooster')
+elif year % 12 == 2:
+    print('Dog')
+elif year % 12 == 3:
+    print('Pig')
+elif year % 12 == 4:
+    print('Rat')
+elif year % 12 == 5:
+    print('Ox')
+elif year % 12 == 6:
+    print('tiger')
+elif year % 12 == 7:
+    print('Hare')
+
+     
