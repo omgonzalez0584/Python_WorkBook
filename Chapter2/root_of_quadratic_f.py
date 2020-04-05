ed08e3b7a7c56d46fadac67a79fd0ec8ee353e4c
omgonzalez0584 <om.gonzalez007@gmail.com>
2020-04-05 11:57:04 -0500

    Exercise 50 , Modificando variables y documentando el codigo

--- Chapter2/root_of_quadratic_f.py
+++ Chapter2/root_of_quadratic_f.py
@@ -1,6 +1,7 @@
 #Exercise 50: Root of a quadratic function
 import math
 
+#Ingresando datos por teclado
 print('Calcule la raiz de una funcion cuadratica')
 a = float(input('Introduzca el valor de a: '))
 b = float(input('Introduzca el valor de b: '))
@@ -13,10 +14,12 @@ discriminante = (b**2) - (4*a*c)
 if discriminante < 0:
     print('El discriminante es menor a cero, por lo tanto no tiene una raiz real.')
 elif discriminante == 0:
-    raiz = -b / (2 * a)
-    print('La raiz real: ' + str(raiz))
+    x = -b / (2 * a)
+    print('Discriminante es igual a cero, solo tiene una raiz.')
+    print('x: ' + str(round(x,4)))
 elif discriminante > 0:
-    raiz1 = (-b + (math.sqrt(discriminante))) / (2 * a)
-    raiz2 = (-b - (math.sqrt(discriminante))) / (2 * a)
-    print('La raiz real 1: ' + str(raiz1))
-    print('La raiz real 2: ' + str(raiz2))
+    x1 = (-b + (math.sqrt(discriminante))) / (2 * a)
+    x2 = (-b - (math.sqrt(discriminante))) / (2 * a)
+    print('discriminante es mayor a cero, tiene dos raices reales (x1 , x2).')
+    print('x1: ' + str(round(x1,4)))
+    print('x2: ' + str(round(x2,4)))
