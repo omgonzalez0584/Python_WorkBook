caf542f3eec834265de5f33783d8c352519cd9bb
omgonzalez0584 <om.gonzalez007@gmail.com>
2020-04-09 17:50:31 -0500

    Try / Except

--- Chapter2/longitud_de_onda.py
+++ Chapter2/longitud_de_onda.py
@@ -1,19 +1,22 @@
 #Exercise 54: Longitud de onda.
 
-longitud_onda = float(input('Introduzca la longitud de onda: '))
+try:
+    longitud_onda = float(input('Introduzca la longitud de onda: '))
 
 
-if longitud_onda >= 380 and longitud_onda < 450:
-    print('La luz es Violeta.')
-elif longitud_onda >= 450  and longitud_onda < 495:
-    print('La luz es Azul.')
-elif longitud_onda >= 495 and longitud_onda < 570:
-    print('La luz es Verde.')
-elif longitud_onda >= 570 and longitud_onda < 590:
-    print('La luz es Amarilla.')
-elif longitud_onda >= 590 and longitud_onda < 620:
-    print('La luz es Naranja.')
-elif longitud_onda >= 620 and longitud_onda <= 750:
-    print('La luz es roja.')
-else:
-    print('Valores esta fuera de rango, debe ingresar valores entre 380 y 750')
+    if longitud_onda >= 380 and longitud_onda < 450:
+        print('La luz es Violeta.')
+    elif longitud_onda >= 450  and longitud_onda < 495:
+        print('La luz es Azul.')
+    elif longitud_onda >= 495 and longitud_onda < 570:
+        print('La luz es Verde.')
+    elif longitud_onda >= 570 and longitud_onda < 590:
+        print('La luz es Amarilla.')
+    elif longitud_onda >= 590 and longitud_onda < 620:
+        print('La luz es Naranja.')
+    elif longitud_onda >= 620 and longitud_onda <= 750:
+        print('La luz es roja.')
+    else:
+        print('Valores esta fuera de rango, debe ingresar valores entre 380 y 750.')
+except ValueError:
+    print('Error!, debes ingresar un valor numerico entre 380 y 750.')
