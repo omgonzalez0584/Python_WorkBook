35004283b54e6eea3c509c458ddb23e3d47538d8
omgonzalez0584 <om.gonzalez007@gmail.com>
2020-04-09 19:35:39 -0500

    Exercise 56, documentando y formateando el outpput

--- Chapter2/cellphone_bill.py
+++ Chapter2/cellphone_bill.py
@@ -1,9 +1,11 @@
 #Exercise 56:  Cellphone bill.
 
+#Input data for keyboard.
 print('The programa calculate a Cellphone Bill.')
 minutes = float(input('Input the amount of minutes: '))
 text_messages = float(input('Input the amount of text-messages: '))
 
+#If/else Statament for calculate the bill.
 if minutes > 50 and text_messages > 50:
     """Calculos si minutos y mensajes son mayores a 50 """
     additional_minutes = minutes - 50
@@ -15,13 +17,13 @@ if minutes > 50 and text_messages > 50:
 
     print('Your Cellphone bill is:')
     print('Base Charge: 15.00')
-    print('Additional minutes: ' + str(additional_minutes))
-    print('Charge for additional minutes: ' + str(recharge_min))
-    print('Additional Text-messages: ' + str(additional_text))
-    print('Charge of additional text-messages: ' + str(recharge_text))
+    print('Additional minutes: ' + str(round(additional_minutes,0)))
+    print('Charge for additional minutes: ' + str(round(recharge_min,2)))
+    print('Additional Text-messages: ' + str(round(additional_text,0)))
+    print('Charge of additional text-messages: ' + str(round(recharge_text,2)))
     print('The 911 support: 0.44')
-    print('The tax 5%:' + str(tax))
-    print('The total is: ' + str(total + tax))
+    print('The tax 5%:' + str(round(tax,2)))
+    print('The total is: ' + str(round((total + tax),2)))
 
 elif minutes > 50 and  text_messages < 50:
     additional_minutes = minutes - 50
@@ -29,11 +31,11 @@ elif minutes > 50 and  text_messages < 50:
     total = 15.00 + recharge_min + 0.44
     tax = total * 0.05
     print('Your Cellphone bill is: ')
-    print('Addtional minutes: ' + str(additional_minutes))
-    print('Charge for additional minutes: ' + str(recharge_min))
+    print('Addtional minutes: ' + str(round(additional_minutes,0)))
+    print('Charge for additional minutes: ' + str(round(recharge_min,2)))
     print('The 911 support: 0.44')
-    print('The tax 5%: ' + str(tax))
-    print('The total is: ' + str(total + tax))
+    print('The tax 5%: ' + str(round(tax,2)))
+    print('The total is: ' + str(round((total + tax),2)))
 
 elif minutes < 50 and text_messages > 50:
     additional_text = text_messages - 50
@@ -41,16 +43,19 @@ elif minutes < 50 and text_messages > 50:
     total = 15.00 + recharge_text + 0.44
     tax = total * 0.05
     print('Your Cellphone bill is: ')
-    print('Additional Text-Messages: ' + str(additional_text))
-    print('Charge for text messages:  ' + str(recharge_text))
+    print('Additional Text-Messages: ' + str(round(additional_text,0)))
+    print('Charge for text messages:  ' + str(round(recharge_text,2)))
     print('The 911 support: 0.44')
-    print('The tax 5%: ' + str(tax))
-    print('The total is: ' + str(total + tax))
+    print('The tax 5%: ' + str(round(tax,2)))
+    print('The total is: ' + str(round((total + tax),2)))
 
 else:
     total = 15.00 + 0.44
     tax = total * 0.05
     print('Your Cellphone bill is: ')
     print('The 911 support: 0.44')
-    print('The tax 5%: ' + str(tax))
-    print('The total is: ' + str(total + tax))
+    print('The tax 5%: ' + str(round(tax,2)))
+    print('The total is: ' + str(round((total + tax),2)))
+
+
+###
