13676a078d3523d2adf91b8322dd8341127b40f5
omgonzalez0584 <om.gonzalez007@gmail.com>
2020-04-11 20:46:36 -0500

    Exercise 61, Documentando el codigo.

--- Chapter3/average.py
+++ Chapter3/average.py
@@ -1,12 +1,17 @@
 #Exercise 61: Average
 
+#Input data for keyboard
 print('Programa calcula el promedio de una lista de valores.')
 value = float(input('Introduzca el valor(Presione 0 para salir): '))
+
+#If/Else Statements.
 if value == 0:
     print('Error, el primer valor no puede ser 0.')
 else:
     suma = value
     contador = 1
+
+#Loop While until that person input 0.
     while True:
         value = float(input('Introduzca el valor(Presione 0 para salir): '))
         if value == 0:
@@ -15,5 +20,6 @@ else:
             suma += value
             contador += 1
 
+#Calculate Average
     promedio = suma / contador
     print('El promedio es: ' + str(promedio))
