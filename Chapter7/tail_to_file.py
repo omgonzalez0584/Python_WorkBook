9c3e9712f701fca7bf6098927988470c09b7d2a4
omgonzalez0584 <om.gonzalez007@gmail.com>
2020-08-23 11:05:06 -0500

    script lee las ultimas lineas de un archivo.

--- Chapter7/tail_to_file.py
+++ Chapter7/tail_to_file.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
-
 import sys
+import os
+
+def tail(lines,line_amount):
+    for line in range((len(lines) -line_amount),len(lines)):
+        print(lines[line])
+
+filename = sys.argv[1]
+line_amount = int(sys.argv[2])
+
+if os.path.exists(filename):
+    with open(filename,'r') as file:
+        lines =  file.readlines()
+        tail(lines,line_amount)
+
+    file.close()
+else:
+    print('El archivo no existe.')
